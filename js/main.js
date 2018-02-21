33f635621e7c98757bf4a6cd1e02992c825c8235
Nathan Lilienthal <nathan@nixpulvis.com>
2018-02-21 10:20:06 -0700

    Scroll below nav.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// Scroll just below the header.
+// NOTE: Maybe just on posts and projects?
+setTimeout(() => {
+  window.scroll(0, 90);
+}, 0);
+
+// Add a "cheat" code.
 Mousetrap.bind('up up down down left right left right b a enter', function() {
   window.location.href = "/MTk3ODkK";
 });
