98cd4c33d846cdb374cc736436af333a55f41bed
Nathan Lilienthal <nathan@nixpulvis.com>
2018-02-21 12:55:46 -0700

    Only scroll the nav, not when the user is further down.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-// Scroll just below the header.
-// NOTE: Maybe just on posts and projects?
-setTimeout(() => {
-  window.scroll(0, 90);
-}, 0);
+window.onload = () => {
+  // Scroll just below the header.
+  // NOTE: Maybe just on posts and projects?
+  if (window.pageYOffset < 90) {
+    window.scroll(0, 90);
+  }
 
-// Add a "cheat" code.
-Mousetrap.bind('up up down down left right left right b a enter', function() {
-  window.location.href = "/MTk3ODkK";
-});
+  // Add a "cheat" code.
+  Mousetrap.bind('up up down down left right left right b a enter', function() {
+    window.location.href = "/MTk3ODkK";
+  });
+};
 
