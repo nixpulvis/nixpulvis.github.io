7d18159f6a2eb3b9dae774868b8c2730a14b9bac
Nathan Lilienthal <nathan@nixpulvis.com>
2018-10-10 19:03:50 -0400

    Navigation 2.0

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
+// TODO: Make this dynamic
+const HEADER_HEIGHT = 90;
+
+function scroll_past_header() {
+  // Don't scroll if we're on the home page,
+  // or if we're past the point anyway.
+  if (window.location.pathname === "/" ||
+      window.pageYOffset >= HEADER_HEIGHT)
+  {
+    return;
+  }
+
+  // Scroll with a smooth animation so the user can see there's a nav header
+  // above them.
+  window.scrollTo({
+    top: HEADER_HEIGHT,
+    behavior: "smooth"
+  })
+}
+
 window.onload = () => {
   // Toggle my hidden, ugly mug.
   document.getElementById("lambda").addEventListener("click", (e) => {
     document.getElementById("me").classList.toggle("hidden");
   });
 
-  // Scroll just below the header.
-  // NOTE: Maybe just on ramblings and projects?
-  if (window.pageYOffset < 90) {
-    window.scroll(0, 90);
-  }
+  // Scroll just below the header on each page but the home page. We will
+  // leave the home page with the navigation header as it's a common use
+  // to navigate from, and it'll help teach the strange behavior.
+  scroll_past_header();
 
   // Add a "cheat" code.
   Mousetrap.bind('up up down down left right left right b a enter', function() {
