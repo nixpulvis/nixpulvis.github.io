0c7b84bc71608ae51e9c67d119639b31ced1b5cb
Nathan Lilienthal <nathan@nixpulvis.com>
2019-09-16 13:56:04 -0400

    Well this is a thing I guess.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ window.onload = () => {
     document.getElementById("me").classList.toggle("hidden");
   });
 
+  // Silly page minimap.
+  pagemap(document.querySelector('#map'), {
+    viewport: null,
+    styles: {
+        'header,footer,section,article': 'rgba(0,0,0,0.08)',
+        'h1,a': 'rgba(0,0,0,0.10)',
+        'h2,h3,h4': 'rgba(0,0,0,0.08)'
+    },
+    back: 'rgba(0,0,0,0.02)',
+    view: 'rgba(0,0,0,0.05)',
+    drag: 'rgba(0,0,0,0.10)',
+    interval: null
+  });
+
   // Add a "cheat" code.
   Mousetrap.bind('up up down down left right left right b a enter', function() {
     window.location.href = "/MTk3ODkK";
